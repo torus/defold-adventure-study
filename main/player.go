b46d24001a73bc9afb984fc3f3326cfef4c03551
Toru Hisai <toru@torus.jp>
2026-01-22 19:56:48 +0900

    put tilemap

--- main/player.go
+++ main/player.go
@@ -27,3 +27,26 @@ embedded_components {
     w: 0.70710677
   }
 }
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"player\"\n"
+  "mask: \"blocker\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 0.5\n"
+  "}\n"
+  ""
+}
